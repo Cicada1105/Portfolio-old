27e78dbc9a61c04910e87404ff4bbe30248fd5b2
Joshua Colvin <joshuacolvin@Joshuas-MacBook-Pro.local>
2020-01-19 23:55:22 -0600

    Implemented design of background with multiple layers in basic HTML

---
+++ my-app/src/Components/bg/canvas.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+class LinearGradient extends React.Component {
+  constructor() {
+    this.id = this.props.elementID;
+  }
+  render() {
+    return (
+
+    )
+  }
+}
+class RedialGradient extends React.Component {
+  render() {
+    return (
+
+    )
+  }
+}
+export { LinearGradient, RadialGradiant }
